5bd160ad2a4f7de24c73377fb1e878edd50a6a7e
khuzaemah <khuza.emah24@gmail.com>
2022-10-26 15:26:23 +0700

    add crud ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Article;
+use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
@@ -14,7 +14,8 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        
+        $articles = Article::latest()->paginate('5');
+        return view('article.index', compact('articles'))->with('i', (request()->input('page', 1) -1) *5);
     }
 
     /**
@@ -24,7 +25,7 @@ class ArticleController extends Controller
      */
     public function create()
     {
-        //
+        return view('article.create');
     }
 
     /**
@@ -35,51 +36,88 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        $input = $request->all();
+
+        if($image = $request->file('image/')){
+            $destinationPath = 'images/';
+            $articleImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $articleImage);
+            $input['image'] = "$articleImage";
+        }
+
+        Article::create($input);
+
+        return redirect()->route('article.index')->with('success', 'Article created successfully.');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Article $article)
     {
-        //
+        return view('article.show', compact('article'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Article $article)
     {
-        //
+        return view('article.edit', compact('article'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Article $article)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        $input = $request->all();
+
+        if($image = $request->file('image')){
+            $destinationPath = 'images/';
+            $articleImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $articleImage);
+            $input['image'] = "$articleImage";
+        }else{
+            unset($input['image']);
+        }
+
+        $article->uppdate($input);
+
+        return  redirect()->route('article.index')->with('success', 'Article updated successfully');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Article $article)
     {
-        //
+        $article->delete();
+
+        return redirect()->route('article.index')->with('success', 'Article deleted successfully');
     }
 }
